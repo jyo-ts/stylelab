4b0417214c9c1d4756ee5e392508f90d9c41273d
EC2 Default User <ec2-user@ip-172-31-16-165.ap-southeast-1.compute.internal>
2019-02-04 16:53:18 +0000

    friends-2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,7 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: 
+    aws_access_key_id:
     aws_secret_access_key: 
     region: 'ap-southeast-1'
   }
@@ -13,23 +13,4 @@ end
 
 
 
-# require 'carrierwave/storage/fog'
-
-
-#     CarrierWave.configure do |config|
-#     if Rails.env.production?
-#     config.storage :fog
-#     config.fog_provider = 'fog/aws'
-#     config.fog_directory = 'stylelab-test-bucket'
-#     config.fog_credentials = {
-#     provider: 'AWS',
-#     aws_access_key_id: 'AKIAJMVD4EUHRKPRJRJQ',
-#     aws_secret_access_key: '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',
-#     region: 'ap-southeast-1',
-#     path_style: true
-#     }
-#     else
-#     config.storage :fog
-#     config.enable_processing = false if Rails.env.test?
-#     end
-# end
+#
